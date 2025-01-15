fe633fc5ef619d167bc580a57da4b97cc3623730
Tara Gorman <ttgorm27@colby.edu>
2025-01-15 15:11:21 -0500

    add assignment number 1

---
+++ assignments/assignment_script_1.R
@@ -0,0 +1,50 @@
+
+source("setup.R")
+
+# load the buoys, the coastline and the database of covariate data
+buoys = gom_buoys()
+coast = read_coastline()
+db = brickman_database()
+
+buoys = buoys |> 
+  filter(id == "M01")  # not3 the double '=='
+
+
+db = DB |>
+  dplyr::filter(scenario == "RCP45", 
+                year == 2055,
+                interval == "mon")
+x = read_brickman(db)
+x
+# read the covariates
+covars = read_brickman(db)
+
+
+x = extract_brickman(covars, buoys, form = "wide")
+
+
+x = x |> 
+  mutate(SST =dT + Tbtm)
+
+# now make a plot of dT as a function of month
+ggplot(data = x,
+       mapping = aes(x = month, y = SST)) +
+  geom_point() + 
+  labs(title = "Temp difference at buoy M01")
+
+
+x = x |>
+  mutate(month = factor(month, levels = month.abb))
+
+# now plot!
+ggplot(data = x,
+       mapping = aes(x = month, y = dT)) +
+  geom_point() + 
+  labs(y = "SST (C)", 
+       title = "RCP4.5 2055 SST at buoy M01")
+
+
+
+# bonus... save the image
+ggsave("images/N01_dT.png")
+
